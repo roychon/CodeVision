c6583db021ce600fa3fe3d8596e7854761602709
OrkhanWebby <ishimairobu.orkhan@gmail.com>
2023-06-16 14:01:51 +0900

    userAddForm

--- view/userAddForm.php
+++ view/userAddForm.php
@@ -7,28 +7,35 @@ ob_start();
 
 <form action="index.php?action=add_user" method="POST">
     <p>
-        <input type="text" name="user_name" placeholder="User Name...">
+        <input type="text" name="user_name" placeholder="Username...">
     </p>
     <p>
-        <span>Not valid User Name</span>
+        <span id="username-not-valid">Not valid Username</span>
+        <span id="username-missing">Username is missing</span>
+        <span>&#8203;</span>
     </p>
     <p>
         <input type="email" name="email" placeholder="E-mail...">
     </p>
     <p>
-        <span>Not valid E-mail</span>
+        <span id="email-not-valid">Not valid E-mail</span>
+        <span id="email-missing">E-mail is missing</span>
+        <span>&#8203;</span>
     </p>
     <p>
         <input type="password" name="password" placeholder="Password...">
     </p>
     <p>
-        <span>Not valid Password</span>
+        <span id="password-not-valid">Not valid Password</span>
+        <span id="password-missing">Password is missing</span>
+        <span>&#8203;</span>
     </p>
     <p>
         <input type="password" name="password_confirm" placeholder="Confirm password...">
     </p>
     <p>
         <span>Does not match with Password</span>
+        <span>&#8203;</span>
     </p>
     <p>
         <input type="submit" value="Sumbit form">
