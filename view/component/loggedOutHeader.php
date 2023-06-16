4113cfa928a93aa556a9f8ccc19e8b92b77d37ca
moonseonyoon <moonseonyoon@gmail.com>
2023-06-16 13:46:47 +0900

    inital commit of logged out header html

---
+++ view/component/loggedOutHeader.php
@@ -0,0 +1,21 @@
+<header>
+    <div>
+        <!-- LEFT SIDE  -->
+        <!-- TODO: Create company logo  -->
+        <img src="company-logo" alt="">
+        <a href="this-is-the-home-link">Home</a>
+
+
+    </div>
+    <div>
+        <!-- MIDDLE  -->
+        <!-- TODO: search bar --->
+        <input type="search" name="" id="">
+    </div>
+    <div>
+        <!-- RIGHT SIDE -->
+        <a href="this-signs-in">Sign In</a>
+        <a href="this-signs-up">Sign Up</a>
+        <!-- nav bar maybe? -->
+    </div>
+</header>
