a7dc2aa35040eb3fcc4a6f6631f02817f4df92a6
moonseonyoon <moonseonyoon@gmail.com>
2023-06-16 15:46:51 +0900

    initial header CSS

--- view/component/loggedOutHeader.php
+++ view/component/loggedOutHeader.php
@@ -1,21 +1,25 @@
-<header>
-    <div>
+<header class="logged-in">
+    <div class="left-side">
         <!-- LEFT SIDE  -->
-        <!-- TODO: Create company logo  -->
-        <img src="company-logo" alt="">
-        <a href="this-is-the-home-link">Home</a>
-
-
+        <!-- TODO: add logo  -->
+        <img class="company-logo" src="" alt="">
+        <!-- TODO:href take us back to index -->
+        <a href="">Home</a>
+        <!-- homebutton -->
     </div>
-    <div>
-        <!-- MIDDLE  -->
-        <!-- TODO: search bar --->
-        <input type="search" name="" id="">
+
+    <div class="search-bar-div">
+        <!-- MIDDLE -->
+        <input class="search-bar" type="search" name="" id="">
+        <i class="fa-solid fa-magnifying-glass"></i>
     </div>
-    <div>
+
+    <div class="right-side">
         <!-- RIGHT SIDE -->
-        <a href="this-signs-in">Sign In</a>
-        <a href="this-signs-up">Sign Up</a>
-        <!-- nav bar maybe? -->
+        <img class="user-profile-pic" src="" alt="">
+        <a href="log-in-btn">Log In</a>
+        <a class="log-out-btn" href="">Log Out</a>
+        <i class="fa-solid fa-user"></i>
+        <!-- nav bar? -->
     </div>
 </header>
