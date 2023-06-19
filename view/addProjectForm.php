b9f50c7eb488edb4f2591b20851ef157db77a564
Roy Chon <roychon040423@gmail.com>
2023-06-19 11:17:41 +0900

    make form template

--- view/addProjectForm.php
+++ view/addProjectForm.php
@@ -45,5 +45,5 @@ ob_start();
 
 <?php
 $content = ob_get_clean();
-require "template.php";
+require "formTemplate.php";
 ?>
---
+++ view/formTemplate.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="./view/addProjectForm.css">
+    <script defer src="./view/js/validateAddProjectForm.js"></script>
+</head>
+
+<body>
+    <?= $content; ?>
+</body>
+
+</html>
