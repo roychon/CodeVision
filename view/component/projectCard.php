63487983107c02747cb87597ec1ac79dcfb8bcfc
moonseonyoon <moonseonyoon@gmail.com>
2023-06-19 15:32:06 +0900

    project card css

--- view/component/projectCard.php
+++ view/component/projectCard.php
@@ -1,13 +1,17 @@
-<div class="card">
-    <h2 class="project-title">Project Title Here</h2>
-    <p>
-        <img class="project-gif" src="" alt="">
-    </p>
-    <div class="bottom-card-container">
-        <!-- this has same class as the header -->
-        <img class="user-profile-pic" src="https://archives.bulbagarden.net/media/upload/f/fb/0001Bulbasaur.png" alt="user project GIF">
-        <span class="language-tag">Langauges Here</span>
-        <!-- TODO: we need to be able to display multiple tags here -->
-
+<div class="main-container">
+    <div class="card">
+        <h2 class="project-title">Project Title Here</h2>
+        <img class="project-gif" src="https://i.pinimg.com/564x/c6/84/28/c68428e616e6985395160bcc868e157f.jpg" alt="project gif">
+        <div class="bottom-card-container">
+            <!-- this has same class as the header -->
+            <img class="user-profile-pic" src="https://i.pinimg.com/564x/3f/93/7e/3f937ecfa588e239dcb8b6fda51c1c3f.jpg" alt="user profile">
+            <div class="user-info">
+                <p>
+                    Username
+                </p>
+                <span class="language-tag">Languages Here</span>
+                <!-- TODO: we need to be able to display multiple tags here -->
+            </div>
+        </div>
     </div>
 </div>
---
+++ view/projectPage.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Project Page";
+ob_start();
+?>
+
+<div class="projectPreview">
+    <!-- VISUAL / GIF -->
+</div>
+<div class="projectCode">
+    <!-- CODE -->
+    <a href="">Edit</a>
+    <a href="">Delete</a>
+</div>
