7af6597b84bbc8d10e80fccaa44cecff77285c54
OrkhanWebby <ishimairobu.orkhan@gmail.com>
2023-06-21 14:32:30 +0900

    adding the MY PROFILE button and connecting it to my profile

--- view/component/loggedInHeader.php
+++ view/component/loggedInHeader.php
@@ -17,11 +17,15 @@
         <!-- TODO:users can click on profile pic to take them to their profile -->
         <img class="user-profile-pic" src="" alt="">
         <!-- TODO:  add the index.php?action=""id="php user id here" -->
-        <?php if (isset($_SESSION)) { ?>
+
+        <!-- MAKING CONNECTION TO MY PROFILE  -->
+        <?php if (isset($_SESSION['id']) and isset($_GET['id']) and $_SESSION['id'] == $_GET['id']) { ?>
             <a href="index.php?action=editUser">Edit Profile</a>
-            <a class="log-out-btn" href="index.php?action=logOut">Log Out</a>
-        <?php
-        } ?>
+        <?php } else { ?>
+            <a href="index.php?action=userProfileView&id=<?= $_SESSION['id'] ?>">My profile</a>
+        <?php } ?>
+        <a class="log-out-btn" href="index.php?action=logOut">Log Out</a>
+
         <i class="fa-solid fa-user"></i>
     </div>
     <!-- nav bar? -->
