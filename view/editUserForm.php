150c5be676d5561e663804065825045fb05315fd
lmusque <lindsey.muscarella@gmail.com>
2023-06-22 13:25:07 +0900

    first commit edit user form CSS

--- view/editUserForm.php
+++ view/editUserForm.php
@@ -3,78 +3,81 @@
 $title = "Edit a user";
 ob_start();
 ?>
+<div id="edit-user-form">
 
-<h1>Edit a user Form</h1>
+    <form action="index.php?action=submitEditedUser" method="POST" id="editForm">
+        <p id="edit-user-title">Edit User</p>
+        <input type="hidden" name="id" value="<?= $id ?>">
+        <p>
+            <label for="firstName">First Name: </label>
+            <input type="text" name="firstName" id="firstName">
+        </p>
+        <p>
+            <span id="firstNameNotValid">Not valid First Name</span>
+            <span id="firstNameMissing">First Name is missing</span>
+            <span>&#8203;</span>
+        </p>
+        <p>
+            <label for="lastName">Last Name: </label>
+            <input type="text" name="lastName" id="lastName">
+        </p>
+        <p>
+            <span id="lastNameNotValid">Not valid Last Name</span>
+            <span id="lastNameMissing">Last Name is missing</span>
+            <span>&#8203;</span>
+        </p>
+        <p>
+            <label for="username">Username: </label>
+            <input type="text" name="username" id="usernameEdit" value="<?= $username ?>">
+        </p>
+        <p>
+            <span id="usernameNotValid">Not valid Username</span>
+            <span id="usernameMissing">Username is missing</span>
+            <span>&#8203;</span>
+        </p>
+        <p>
+            <label for="email">E-mail: </label>
+            <input type="text" name="email" id="emailEdit" value="<?= $email ?>">
+        </p>
+        <p>
+            <span id="emailNotValid">Not valid E-mail</span>
+            <span id="emailMissing">E-mail is missing</span>
+            <span>&#8203;</span>
+        </p>
+        <p>
+            <label for="password">Password: </label>
+            <input type="text" name="password" id="passwordEdit">
+        </p>
+        <p>
+            <span id="passwordNotValid">Not valid Password</span>
+            <span id="passwordMissing">Password is missing</span>
+            <span>&#8203;</span>
+        </p>
+        <p>
+            <label for="profileImage">Profile Image: </label>
+            <input type="text" name="profileImage" id="profileImage">
+        </p>
+        <p>
+            <label for="bio">Bio: </label>
+            <input type="text" name="bio" id="bio">
+        </p>
+        <p>
+            <label for="linkedIn">LinkedIn: </label>
+            <input type="text" name="linkedIn" id="linkedIn">
+        </p>
+        <p>
+            <label for="gitHub">GitHub: </label>
+            <input type="text" name="gitHub" id="gitHub">
+        </p>
 
-<form action="index.php?action=submitEditedUser" method="POST" id="editForm">
-    <input type="hidden" name="id" value="<?= $id ?>">
-    <p>
-        <label for="firstName">First Name: </label>
-        <input type="text" name="firstName" id="firstName">
-    </p>
-    <p>
-        <span id="firstNameNotValid">Not valid First Name</span>
-        <span id="firstNameMissing">First Name is missing</span>
-        <span>&#8203;</span>
-    </p>
-    <p>
-        <label for="lastName">Last Name: </label>
-        <input type="text" name="lastName" id="lastName">
-    </p>
-    <p>
-        <span id="lastNameNotValid">Not valid Last Name</span>
-        <span id="lastNameMissing">Last Name is missing</span>
-        <span>&#8203;</span>
-    </p>
-    <p>
-        <label for="username">Username: </label>
-        <input type="text" name="username" id="usernameEdit" value="<?= $username ?>">
-    </p>
-    <p>
-        <span id="usernameNotValid">Not valid Username</span>
-        <span id="usernameMissing">Username is missing</span>
-        <span>&#8203;</span>
-    </p>
-    <p>
-        <label for="email">E-mail: </label>
-        <input type="text" name="email" id="emailEdit" value="<?= $email ?>">
-    </p>
-    <p>
-        <span id="emailNotValid">Not valid E-mail</span>
-        <span id="emailMissing">E-mail is missing</span>
-        <span>&#8203;</span>
-    </p>
-    <p>
-        <label for="password">Password: </label>
-        <input type="text" name="password" id="passwordEdit">
-    </p>
-    <p>
-        <span id="passwordNotValid">Not valid Password</span>
-        <span id="passwordMissing">Password is missing</span>
-        <span>&#8203;</span>
-    </p>
-    <p>
-        <label for="profileImage">Profile Image: </label>
-        <input type="text" name="profileImage" id="profileImage">
-    </p>
-    <p>
-        <label for="bio">BIO: </label>
-        <input type="text" name="bio" id="bio">
-    </p>
-    <p>
-        <label for="linkedIn">LinkedIn: </label>
-        <input type="text" name="linkedIn" id="linkedIn">
-    </p>
-    <p>
-        <label for="gitHub">GitHub: </label>
-        <input type="text" name="gitHub" id="gitHub">
-    </p>
-    <p>
-        <input type="submit" value="Submit the changes" id="editSubmit">
-    </p>
+        <p>
+            <input type="submit" value="submit changes" id="editSubmit">
+        </p>
 
 
-</form>
+
+    </form>
+</div>
 <?php
 $content = ob_get_clean();
 require "template.php";
