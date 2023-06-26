0b5068a2299b96218777213d8a84062381226f83
Roy Chon <roychon040423@gmail.com>
2023-06-26 16:10:13 +0900

    add more details to carousel

--- view/component/carousel.php
+++ view/component/carousel.php
@@ -1,9 +1,9 @@
-<div class="slide" style="background-image: url(<?= $carousels[$i]->gif ?>)" <?php if ($i == 0) echo " data-active" ?>>
+<div class="slide" style="background-image: url(<?= $carousels[$i]->gif ?>)" <?php if ($i == 0) echo "data-active" ?>>
     <div class="flex">
         <img class="carousel-profile-img" src="<?= $carousels[$i]->profile_img ?>" alt="">
         <p class="carousel-username"><?= $carousels[$i]->username ?></p>
     </div>
     <h1><?= $carousels[$i]->title ?></h1>
     <p class="carousel-description"><?= $carousels[$i]->description ?></p>
-    <p><button>More Details</button></p>
+    <button class="viewMore" data-projectid = <?= $carousels[$i]->project_id ?>>More Details</button>
 </div>
