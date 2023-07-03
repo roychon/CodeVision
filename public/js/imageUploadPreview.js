9deb47f516a50b7b46711e900b24752bce80be61
RobMateer <robertmateer51@gmail.com>
2023-07-03 16:11:58 +0900

    previewing uploaded images from profile

--- public/js/imageUploadPreview.js
+++ public/js/imageUploadPreview.js
@@ -6,34 +6,3 @@ function showPreview(event) {
         preview.style.display = "block";
     }
 }
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $("#imagepreview1").prop("src", e.target.result).show();
-        };
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$("#input1").change(function () {
-    readURL(this);
-    $("#imagepreview1").show();
-});
-
-$("#input1").click(function () {
-    $("#imagepreview1").attr("src", "");
-});
-
-$("#imagepreview1").click(function () {
-    $("#input1").replaceWith($("#input1").clone(true));
-    $("#imagepreview1").hide();
-});
-
-$("#cancel").click(function (e) {
-    $("#input1").val("");
-    $("#imagepreview1").attr("src", "");
-});
