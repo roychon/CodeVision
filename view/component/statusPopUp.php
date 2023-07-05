4c6cac6126458da5b01920e784d7476c7c2a967e
n444v <122508317+n444v@users.noreply.github.com>
2023-07-05 11:08:22 +0900

    css fixes

--- view/component/statusPopUp.php
+++ view/component/statusPopUp.php
@@ -12,7 +12,6 @@ if (isset($_GET['error']) and $_GET['error'] == "true") {
     <div class="dialog-content">
         <p class="dialog-message"><?= $_GET['message'] ?></p>
         <button class="confirm-btn" onclick="closeModal()">Confirm</button>
-        <button class="close-btn" onclick="closeModal()">Close</button>
     </div>
 
 </dialog>
--- view/signInForm.php
+++ view/signInForm.php
@@ -30,7 +30,7 @@ ob_start();
       <input type="text" name="password" id="loginPassword">
     </p>
 
-    <a class="sign-assist" href="index.php?action=">Need help signing in?</a>
+    <!-- <a class="sign-assist" href="index.php?action=">Need help signing in?</a> -->
 
 
     <p>
